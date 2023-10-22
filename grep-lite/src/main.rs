ce37d99faee3e8b85a0382b35b53e5d637575856
mohadang <mohadang@outlook.com>
2023-10-22 17:23:39 +0900

    [mod] print matching lines(cur, prev, next)

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,15 +1,43 @@
-
 fn main() {
-    let search_term = "picture";
-    let quote = "\
-Every face, every shop, bedroom window, public-house, and
+    let context_lines = 1;
+    let needle = "oo";
+    let haystack = "\
+Every face, every shop, 
+bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
-What do we seek through millions of pages?";      
+What do we seek 
+through millions of pages?";
 
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            println!("{}: {}", i, line);
+    let mut tags: Vec<usize> = Default::default();
+    let mut ctx: Vec<Vec<(usize, String)>> = Vec::new();
+    for (i, line) in haystack.lines().enumerate() {
+        if line.contains(needle) {
+            tags.push(i);
+            ctx.push(Vec::with_capacity(2 * context_lines + 1));
         }
+    }
+
+    if tags.is_empty() {
+        return;
+    }
+
+    for (i, line) in haystack.lines().enumerate() {
+        for (j, tag) in tags.iter().enumerate() {
+            let lower_bound = tag.saturating_sub(context_lines); //instead of overflow
+            let upper_bound = tag + context_lines;
+            if (lower_bound <= i) && (i <= upper_bound) {
+                let local_ctx = (i, String::from(line));
+                ctx[j].push(local_ctx);
+            }
+        }
+    }
+
+    for local_ctx in ctx.iter() {
+        for &(i, ref line) in local_ctx.iter() {
+            let line_num = i + 1;
+            println!("{}: {}", line_num, line);
+        }
+        println!("");
     }
 }
