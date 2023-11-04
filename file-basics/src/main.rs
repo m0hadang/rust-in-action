67055814fc9073cd00eb061d64ef675a7a61a2a9
mohadang <mohadang@outlook.com>
2023-11-04 14:53:48 +0900

    [mod] Error handling using Result

--- file-basics/src/main.rs
+++ file-basics/src/main.rs
@@ -1,5 +1,11 @@
 #![allow(unused_variables)]
 
+use rand::prelude::*;
+
+fn one_in(denominator: u32) -> bool {
+    thread_rng().gen_ratio(1, denominator)
+}
+
 
 #[derive(Debug)]
 struct File {
@@ -25,41 +31,49 @@ impl File {
     fn read(
         self: &File,
         save_to: &mut Vec<u8>
-        ) -> usize {
+        ) -> Result<usize, String> {
 
         let mut tmp = self.data.clone();
         let read_length = tmp.len();
         save_to.reserve(read_length);
         save_to.append(&mut tmp);
-        read_length
+        Ok(read_length)
     }
 }
 
 
-fn open(f: &mut File) -> bool {
-    true
+fn open(f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Permission denied");
+        return Err(err_msg);
+    }
+    Ok(f)
 }
 
-fn close(f: &mut File) -> bool {
-    true
+fn close(f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Interrupted by signal!");
+        return Err(err_msg);
+    }
+    Ok(f)
 }
 
 
 fn main() {
-    let f3_data: Vec<u8> = vec![
+    let data: Vec<u8> = vec![
         114, 117, 115, 116, 33
     ];
-    let mut f3 = File::new_with_data("2.txt", &f3_data);
+    let mut f = File::new_with_data("2.txt", &data);
 
     let mut buffer: Vec<u8> = vec![];
 
-    open(&mut f3);
-    let f3_length = f3.read(&mut buffer);
-    close(&mut f3);
+    f = open(f).unwrap();
+    let f_length = f.read(&mut buffer).unwrap();
+    f = close(f).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
 
-    println!("{:?}", f3);
-    println!("{} is {} bytes long", &f3.name, f3_length);
+    println!("{:?}", f);
+    println!("{} is {} bytes long", &f.name, f_length);
     println!("{}", text);
 }
