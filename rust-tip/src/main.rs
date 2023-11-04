8fc9a15c74b09dd3739cef561aee197ff0510dad
mohadang <mohadang@outlook.com>
2023-11-04 11:47:11 +0900

    [add] example of from_utf8_lossy

--- rust-tip/src/main.rs
+++ rust-tip/src/main.rs
@@ -2,10 +2,18 @@
 
 struct Hostname(String); // new type
 
-fn connect(host: Hostname) {
+fn ex_new_type_patter(host: Hostname) {
     println!("connected to {}", host.0);
 }
 
+fn ex_print_from_utf8_lossy() {
+    // some bytes, in a vector
+    let sparkle_heart = vec![240, 159, 146, 150];
+    let sparkle_heart = String::from_utf8_lossy(&sparkle_heart);
+    assert_eq!("💖", sparkle_heart);
+}
+
 fn main() {
-    //connect("aaa"); // cant use
+    //ex_new_type_patter("aaa"); // protect wrong use(it's type check)
+    ex_print_from_utf8_lossy();
 }
