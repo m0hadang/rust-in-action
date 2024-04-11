0c1cfe0f061056579ce3e29845b311cff51ee88f
mohadang <mohadang@outlook.com>
2024-04-12 05:24:53 +0900

    [mod] add disk indexing function for actionkv

--- actionkv/src/akv-mem.rs
+++ actionkv/src/akv-mem.rs
@@ -1,58 +1,79 @@
-use core::panic;
+use std::collections::HashMap;
 
 use libactionkv::ActionKV;
 
 #[cfg(target_os = "windows")]
 const USAGE: &str = "
 Usage:
-akv_mem.exe FILE get KEY
-akv_mem.exe FILE delete KEY
-akv_mem.exe FILE insert KEY VALUE
-akv_mem.exe FILE update KEY VALUE
+akv_disk.exe FILE get KEY
+akv_disk.exe FILE delete KEY
+akv_disk.exe FILE insert KEY VALUE
+akv_disk.exe FILE update KEY VALUE
 ";
 
 #[cfg(not(target_os = "windows"))]
 const USAGE: &str = "
 Usage:
-akv_mem FILE get KEY
-akv_mem FILE delete KEY
-akv_mem FILE insert KEY VALUE
-akv_mem FILE update KEY VALUE
+akv_disk FILE get KEY
+akv_disk FILE delete KEY
+akv_disk FILE insert KEY VALUE
+akv_disk FILE update KEY VALUE
 ";
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 4 {
-        panic!("{USAGE}");
-    }
+type ByteStr = [u8];
+type ByteString = Vec<u8>;
 
-    let file_name = args[1].as_str();
-    let action = args[2].as_ref();
-    let key = args[3].as_ref();
+fn store_index_on_disk(a: &mut ActionKV, index_key: &ByteStr) {
+    a.index.remove(index_key);
+    let index_as_bytes = bincode::serialize(&a.index).unwrap();
+    a.index = std::collections::HashMap::new();
+    a.insert(index_key, &index_as_bytes).unwrap();
+}
+
+fn main() {
+    const INDEX_KEY: &ByteStr = b"+index";
+
+    let args: Vec<String> = std::env::args().collect();
+    let fname = args.get(1).expect(&USAGE);
+    let action = args.get(2).expect(&USAGE).as_ref();
+    let key = args.get(3).expect(&USAGE).as_ref();
     let maybe_value = args.get(4);
 
-    let path = std::path::Path::new(file_name);
-    let mut store = ActionKV::open(path).expect("unable to open file");
-    store.load().expect("unable to load data");
+    let path = std::path::Path::new(&fname);
+    let mut a = ActionKV::open(path).expect("unable to open file");
+
+    a.load().expect("unable to load data");
 
     match action {
-        "get" => match store.get(key).unwrap() {
-            None => eprintln!("{:?} not found", key),
-            Some(value) => println!("{:?}", value),
-        },
+        "get" => {
+            let index_as_bytes = a.get(&INDEX_KEY).unwrap().unwrap();
 
-        "delete" => store.delete(key).unwrap(),
+            let index_decoded = bincode::deserialize(&index_as_bytes);
+
+            let index: HashMap<ByteString, u64> = index_decoded.unwrap();
+
+            match index.get(key) {
+                None => eprintln!("{:?} not found", key),
+                Some(&i) => {
+                    let kv = a.get_at(i).unwrap();
+                    println!("{:?}", kv.value)
+                }
+            }
+        }
+
+        "delete" => a.delete(key).unwrap(),
 
         "insert" => {
-            let value = maybe_value.expect(USAGE).as_ref();
-            store.insert(key, value).unwrap()
+            let value = maybe_value.expect(&USAGE).as_ref();
+            a.insert(key, value).unwrap();
+            store_index_on_disk(&mut a, INDEX_KEY);
         }
 
         "update" => {
-            let value = maybe_value.expect(USAGE).as_ref();
-            store.update(key, value).unwrap()
+            let value = maybe_value.expect(&USAGE).as_ref();
+            a.update(key, value).unwrap();
+            store_index_on_disk(&mut a, INDEX_KEY);
         }
-
-        _ => eprintln!("{}", USAGE),
+        _ => eprintln!("{}", &USAGE),
     }
 }
