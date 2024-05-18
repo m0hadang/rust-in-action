b77acdd9a172c7f536cf1995212644a78ba12091
mohadang <ehdlaos@gmail.com>
2024-05-18 17:25:51 +0900

    [mod] add set clock function(windows system)

--- clock/src/main.rs
+++ clock/src/main.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Days, Local, TimeZone};
+use chrono::{Datelike, DateTime, Days, Local, Timelike, TimeZone};
 use std::mem::zeroed;
 
 #[cfg(not(windows))]
@@ -18,6 +18,49 @@ fn set_clock<Tz: TimeZone>(t: DateTime<Tz>) {
     unsafe {
         let mock_tz: *const timezone = std::ptr::null();
         settimeofday(&u as *const timeval, mock_tz);
+    }
+}
+
+#[cfg(windows)]
+fn set_clock<Tz: TimeZone>(t: DateTime<Tz>) -> () {
+    use chrono::Weekday;
+    use kernel32::SetSystemTime;
+    use winapi::{SYSTEMTIME, WORD};
+
+    let t = t.with_timezone(&Local);
+
+    let mut systime: SYSTEMTIME = unsafe { zeroed() };
+
+    let dow = match t.weekday() {
+        Weekday::Mon => 1,
+        Weekday::Tue => 2,
+        Weekday::Wed => 3,
+        Weekday::Thu => 4,
+        Weekday::Fri => 5,
+        Weekday::Sat => 6,
+        Weekday::Sun => 0,
+    };
+
+    let mut ns = t.nanosecond();
+    let is_leap_second = ns > 1_000_000_000;
+
+    if is_leap_second {
+        ns -= 1_000_000_000;
+    }
+
+    systime.wYear = t.year() as WORD;
+    systime.wMonth = t.month() as WORD;
+    systime.wDayOfWeek = dow as WORD;
+    systime.wDay = t.day() as WORD;
+    systime.wHour = t.hour() as WORD;
+    systime.wMinute = t.minute() as WORD;
+    systime.wSecond = t.second() as WORD;
+    systime.wMilliseconds = (ns / 1_000_000) as WORD;
+
+    let system_time_ptr: *const SYSTEMTIME = &systime as *const SYSTEMTIME;
+
+    unsafe {
+        SetSystemTime(system_time_ptr);
     }
 }
 
