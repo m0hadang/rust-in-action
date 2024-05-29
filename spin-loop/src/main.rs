c76d29fc84fc65b6765247080924613456208303
mohadang <mohadang@outlook.com>
2024-05-30 00:08:37 +0900

    [add] spin-loop

---
+++ spin-loop/src/main.rs
@@ -0,0 +1,32 @@
+use std::{thread, time};
+fn spin_loop() {
+    for n in 1..1001 {
+        let mut handlers: Vec<thread::JoinHandle<()>> = Vec::with_capacity(n);
+
+        let start = time::Instant::now();
+        for _m in 0..n {
+            let handle = thread::spawn(|| {
+                let start = time::Instant::now();
+                let pause = time::Duration::from_millis(20);
+                while start.elapsed() < pause {
+                    thread::yield_now();
+                }
+                /*
+                 * - sleep version
+                 * thread::sleep(time::Duration::from_millis(20))
+                 * */
+            });
+            handlers.push(handle);
+        }
+
+        while let Some(handle) = handlers.pop() {
+            handle.join();
+        }
+
+        let finish = time::Instant::now();
+        println!("{}\t{:02?}", n, finish.duration_since(start));
+    }
+}
+fn main() {
+    spin_loop();
+}
